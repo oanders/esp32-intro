b377a16c3f857fe8c043bd70fa287313b5f6fa06
Oskar Andersson Reyna <oanders@kth.se>
2024-05-19 20:50:27 +0200

    6.3 Alertor

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,54 @@
 #include <Arduino.h>
 
-#define PIN_LED  2
-#define PIN_BUTTON 13
+#define PIN_BUZZER  13
+#define PIN_BUTTON  4
+#define CHN         0
+hw_timer_t * timer = NULL;
 
-byte ledPins[] = {15, 2, 0, 4, 5, 18, 19, 21, 22, 23};
-int ledCounts;
+bool isAlerting = false;
+
+void IRAM_ATTR onTimer(){
+  digitalWrite(PIN_BUZZER, !digitalRead(PIN_BUZZER));
+}
 
 
 void setup() {
-  ledCounts = sizeof(ledPins);
-  for (int i = 0; i < ledCounts; i++){
-    pinMode(ledPins[i], OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
+
+  timer = timerBegin(0, 80, true);
+
+  timerAttachInterrupt(timer, &onTimer, true);
+}
+
+void alert(){
+  float sinVal;
+  int toneVal;
+  for (size_t x = 0; x < 360; x += 10)
+  {
+    sinVal = sin(x*(PI/180));
+    toneVal = 2000 + sinVal * 500;
+    timerAlarmWrite(timer, 500000/toneVal, true);
+    delay(1);
   }
+  
+
 }
 
 void loop() {
-  for (int i = 0; i < ledCounts; i++){
-    digitalWrite(ledPins[i], HIGH);
-    delay(100);
-    digitalWrite(ledPins[i], LOW);
-  }
-  for (int i = ledCounts -1; i > -1; i--){
-    digitalWrite(ledPins[i], HIGH);
-    delay(100);
-    digitalWrite(ledPins[i], LOW);
+  if(digitalRead(PIN_BUTTON) == LOW){
+    if(!isAlerting){
+      isAlerting = true;
+      timerAlarmWrite(timer, 1000, true);
+      timerAlarmEnable(timer);
+    }
+    alert();
+  }else{
+    if(isAlerting){
+      isAlerting = false;
+      timerAlarmDisable(timer);
+      digitalWrite(PIN_BUZZER, LOW);
+    }
   }
 }
 
